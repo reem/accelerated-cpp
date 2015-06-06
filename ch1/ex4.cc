c0bbe332d337d15324ff5bb3cbd05fec94426d2c
Jonathan Reem <jonathan.reem@gmail.com>
2015-06-05 17:41:21 -0700

    (exercise) Add exercise 0-4.

---
+++ ch1/ex4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout
+        << "// A small C++ program." << std::endl
+        << "#include <iostream>" << std::endl
+        << std::endl
+        << "int main() {" << std::endl
+        << "    std::cout << \"Hello World!\" << std::endl;" << std::endl
+        << "    return 0;" << std::endl
+        << "}" << std::endl
+    << std::endl;
+    return 0;
+}
+
